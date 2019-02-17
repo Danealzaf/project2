66194a66000b1a128e649e26d26cc9cc389a8f8a
Danealzaf <47616064+Danealzaf@users.noreply.github.com>
2019-02-17 12:33:03 +0800

    fisrt commit

---
+++ src/javaapplication2/NewClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Daneal
+ */
+public class NewClass {
+       public static void main(String [] args){
+           String peserta="Pelajar Sains Komputer";
+           int tahun=2017;
+           char jantina='l';
+           
+           System.out.println("Peserta:"+peserta);
+           System.out.println("Tahun:"+tahun);
+           System.out.println("jantina"+jantina);
+       }    
+}
